a5aa86211ba598b8107b557c922073b33e6fc433
Pratik parekh <pratikparekh217@gmail.com>
2019-06-28 16:00:52 -0400

    Loading-false

--- src/post/NewPost.js
+++ src/post/NewPost.js
@@ -33,7 +33,7 @@ class NewPost extends Component {
 		const {title,body,fileSize}=this.state
 
 		if(fileSize>1000000){
-			this.setState({error:"FileSize should be less than 1000kb",loading:false"})
+			this.setState({error:"FileSize should be less than 1000kb",loading:false})
 			return false;
 		}
 
